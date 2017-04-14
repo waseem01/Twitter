307ab5566814db2fca64c97d8cffd6ab0e36261c
waseemm <wmohd@CHIC02QRGD3G8WN.sea.corp.expecn.com>
2017-04-13 23:15:39 -0500

    tweet pop

--- Twitter/Controllers/TweetsViewController.swift
+++ Twitter/Controllers/TweetsViewController.swift
@@ -36,6 +36,17 @@ class TweetsViewController: UIViewController, UITableViewDelegate, UITableViewDa
             print(error)
         }
     }
+    
+    @IBAction func tweetTapped(_ sender: UIBarButtonItem) {
+        let animationView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width-30, height: 300))
+        animationView.center = view.center
+        animationView.backgroundColor = .red
+        animationView.transform = CGAffineTransform(scaleX: 0, y: 0)
+        self.view!.addSubview(animationView)
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .curveEaseInOut, animations: {() -> Void in
+            animationView.transform = .identity
+        }, completion: { _ in })
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tweets.count
